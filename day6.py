ef57f74221396e3d51528382dd6f40f1bad0b2c0
unknown <punyashree.gg.2007@gmail.com>
2026-02-20 20:17:49 +0530

    Day6

---
+++ day6.py
@@ -0,0 +1,9 @@
+# check the number if positive, negative, or zero
+p = int(input("Enter the Number\n"))
+if p==0:
+    print("Entered number is Zero")
+else:
+    if p>0:
+        print("Entered number is Positive")
+    else:
+        print("Entered nymber is Negative")
