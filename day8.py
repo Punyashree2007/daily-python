62175262655ba9cd4d0897ff094d65ff4e966b95
unknown <punyashree.gg.2007@gmail.com>
2026-02-24 05:02:16 +0530

    Day8

---
+++ day8.py
@@ -0,0 +1,16 @@
+# simple calculator
+print("Simple Calulator")
+p1 = int(input("Enter p1 value"))
+p2 = int(input("Enter p2 value"))
+q = p1+p2 # Addition
+r = p1-p2 # subtraction
+s = p1*p2 # multiplication
+t = p1/p2 # division
+u = p1**p2 # power
+v = p1%p2 #reminder
+print("add",q)
+print("sub",r)
+print("mult",s)
+print("div",t)
+print("power",u)
+print("rem",v)
