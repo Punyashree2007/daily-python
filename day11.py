3902f32709caddb12dc58df80f55401dd761e922
unknown <punyashree.gg.2007@gmail.com>
2026-02-25 04:16:35 +0530

    Day11

---
+++ day11.py
@@ -0,0 +1,9 @@
+# finding square and cube of a number
+a = int(input("enter a number\n"))
+pu = int(input("Enter the Choice\n"))
+if pu==1:
+    print("Square:",a*a)
+elif pu==2:
+    print("Cube:",a*a*a)
+else:
+    print("invalid")
