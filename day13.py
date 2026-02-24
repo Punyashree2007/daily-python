1e71a63ed81107dfeedb1adc0d4315bcebfd6b0b
unknown <punyashree.gg.2007@gmail.com>
2026-02-25 04:28:42 +0530

    Day13

---
+++ day13.py
@@ -0,0 +1,6 @@
+#c check the char is vowel
+alp = input("Enter the Character\n")
+if alp=='a' or alp=='e' or alp=='i' or alp=='o' or alp=='u':
+    print("Entered char is a Vowel")
+else:
+    print("Not Vowel")
