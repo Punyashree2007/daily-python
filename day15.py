835d57619cef45fbb78733c5495b243cd3eed45e
unknown <punyashree.gg.2007@gmail.com>
2026-02-26 22:49:51 +0530

    Day15

---
+++ day15.py
@@ -0,0 +1,11 @@
+# find smallest amond 3 numbers
+a = 10
+b = 16
+c = 0.1
+if a<b and a<c:
+    print("a is smallest")
+elif b<a and b<c:
+    print("b is Smallest")
+else:
+    print("c is smallest")
+    
